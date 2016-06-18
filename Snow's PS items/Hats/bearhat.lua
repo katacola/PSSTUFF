383e5b15bea98d3d17c56e228c4b986752a0feb3
katacola <katacola453@gmail.com>
2016-06-18 20:37:15 +1000

    Add files via upload

---
+++ Snow's PS items/Hats/bearhat.lua
@@ -0,0 +1,22 @@
+ITEM.Name = 'Bear Hat'
+ITEM.Price = 0
+ITEM.Model = 'models/captainbigbutt/skeyler/hats/bear_hat.mdl'
+ITEM.Attachment = 'eyes'
+
+function ITEM:OnEquip(ply, modifications)
+	ply:PS_AddClientsideModel(self.ID)
+end
+
+function ITEM:OnHolster(ply)
+	ply:PS_RemoveClientsideModel(self.ID)
+end
+
+function ITEM:ModifyClientsideModel(ply, model, pos, ang)
+	model:SetModelScale(1, 0)
+	pos = pos + (ang:Forward() * -3.264) + (ang:Right() * 0.095) +  (ang:Up() * 2.955)
+	ang:RotateAroundAxis(ang:Right(), 0)
+	ang:RotateAroundAxis(ang:Up(), 0)
+	ang:RotateAroundAxis(ang:Forward(), 0)
+	
+	return model, pos, ang
+end
