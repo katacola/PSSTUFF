dbe5a796dadcb7c44eee1e459a4a4d42cd7717f0
katacola <katacola453@gmail.com>
2016-06-18 19:43:20 +1000

    Create fedora.lua

---
+++ Snow's PS items/Hats/fedora.lua
@@ -0,0 +1,22 @@
+ITEM.Name = 'Fedora'
+ITEM.Price = 0
+ITEM.Model = 'models/modified/hat01_fix.mdl'
+ITEM.Attachment = 'eyes'
+
+function ITEM:OnEquip(ply, modifications)
+	ply:PS_AddClientsideModel(self.ID)
+end
+
+function ITEM:OnHolster(ply)
+	ply:PS_RemoveClientsideModel(self.ID)
+end
+
+function ITEM:ModifyClientsideModel(ply, model, pos, ang)
+	model:SetModelScale(1, 0)
+	pos = pos + (ang:Forward() * -4.428) + (ang:Right() * -0.076) +  (ang:Up() * 1.011)
+	ang:RotateAroundAxis(ang:Right(), 0)
+	ang:RotateAroundAxis(ang:Up(), 0)
+	ang:RotateAroundAxis(ang:Forward(), 0)
+	
+	return model, pos, ang
+end
